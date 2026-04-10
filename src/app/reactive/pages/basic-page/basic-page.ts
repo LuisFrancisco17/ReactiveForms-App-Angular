880dcd78d2498d5bf5db62a866ff3fdbd6b9a13f
Luis Gil <luisfrancisco17msn@gmail.com>
2026-04-09 23:25:30 -0500

    Implementación de formularios dinamicos con arreglos

--- src/app/reactive/pages/basic-page/basic-page.ts
+++ src/app/reactive/pages/basic-page/basic-page.ts
@@ -24,33 +24,6 @@ export class BasicPage {
     inStorage: [0, [Validators.required, Validators.min(0)]],
   });
 
-  // myForm2 = new FormGroup({
-  //   name: new FormControl(''),
-  //   price: new FormControl(0),
-  //   inStorage: new FormControl(0),
-  // });
-
-  // isValidField(fieldName: string): boolean | null {
-  //   return this.myForm.controls[fieldName].errors && this.myForm.controls[fieldName].touched;
-  // }
-
-  // getFieldError(fieldName: string): string | null {
-  //   if (!this.myForm.controls[fieldName]) return null;
-
-  //   const errors = this.myForm.controls[fieldName].errors ?? {};
-  //   for (const key of Object.keys(errors)) {
-  //     switch (key) {
-  //       case 'required':
-  //         return 'Este campo es requerido';
-  //       case 'minlength':
-  //         return `Mínimo de ${errors['minlength'].requiredLength} caracteres`;
-  //       case 'min':
-  //         return `Valor mínimo de ${errors['min'].min}`;
-  //     }
-  //   }
-  //   return null;
-  // }
-
   onSave() {
     if (this.myForm.invalid) {
       this.myForm.markAllAsTouched();
--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -1,14 +1,9 @@
-import { FormGroup } from '@angular/forms';
+import { FormArray, FormGroup, ValidationErrors } from '@angular/forms';
 
 export class FormUtils {
   // Expresiones regulares
-  static isValidField(form: FormGroup, fieldName: string): boolean | null {
-    return !!form.controls[fieldName].errors && form.controls[fieldName].touched;
-  }
-  static getFieldError(form: FormGroup, fieldName: string): string | null {
-    if (!form.controls[fieldName]) return null;
 
-    const errors = form.controls[fieldName].errors ?? {};
+  static getTextError(errors: ValidationErrors) {
     for (const key of Object.keys(errors)) {
       switch (key) {
         case 'required':
@@ -21,6 +16,26 @@ export class FormUtils {
     }
     return null;
   }
-}
 
-// FormUtils.isValidField()
+  static isValidField(form: FormGroup, fieldName: string): boolean | null {
+    return !!form.controls[fieldName].errors && form.controls[fieldName].touched;
+  }
+  static getFieldError(form: FormGroup, fieldName: string): string | null {
+    if (!form.controls[fieldName]) return null;
+
+    const errors = form.controls[fieldName].errors ?? {};
+
+    return FormUtils.getTextError(errors);
+  }
+  static isValidFieldInArray(formArray: FormArray, index: number) {
+    return formArray.controls[index].errors && formArray.controls[index].touched;
+  }
+
+  static getFieldErrorInArray(formArray: FormArray, index: number): string | null {
+    if (formArray.controls.length === 0) return null;
+
+    const errors = formArray.controls[index].errors ?? {};
+
+    return FormUtils.getTextError(errors);
+  }
+}
