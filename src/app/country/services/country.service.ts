bd7cf3cbbe937358de9484e15fc6bdd43df0a28a
Luis Gil <luisfrancisco17msn@gmail.com>
2026-04-19 00:04:56 -0500

    Implementación de selector de borders

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { combineLatest, Observable, of } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
 
 @Injectable({ providedIn: 'root' })
@@ -26,5 +26,13 @@ export class CountryService {
     const url = `${this.baseUrl}/alpha/${alphaCode}?fields=cca3,name,borders`;
     return this.http.get<Country>(url);
   }
-  getCountryBorderByCodes(borders: string[]) {}
+  getCountryNamesByCodeArray(countryCodes: string[]): Observable<Country[]> {
+    if (!countryCodes || countryCodes.length === 0) return of([]);
+    const countriesRequests: Observable<Country>[] = [];
+    countryCodes.forEach((code) => {
+      const request = this.getCountryByAlphaCode(code);
+      countriesRequests.push(request);
+    });
+    return combineLatest(countriesRequests);
+  }
 }
